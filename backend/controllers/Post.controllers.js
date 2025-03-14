e98ec17318ff930964010b9aec8660b2be018c54
khalil-t <khaliltouil123s@gmail.com>
2025-03-14 02:46:02 +0100

    feat: add new functionality ReactOnPost

--- backend/controllers/Post.controllers.js
+++ backend/controllers/Post.controllers.js
@@ -113,6 +113,41 @@ catch (error) {
 
 }
 
+export const ReactOnPost = async(req, res)=>{
+try{
+    const {id} =req.params
+    const userId = req.user._id;
+
+    if (!id) {
+        return res.status(400).json({ error: "Post ID is required" });
+    }
+    const post = await Post.findById(id)
+    
+    if(!post){
+    return res.status(404).json({ error: "Post not found" });
+    }
+    
+    const hasLiked = post.likes.includes(userId);
+
+    if (hasLiked) {
+        post.likes = post.likes.filter((like) => like.toString() !== userId.toString());
+    } else {
+        post.likes.push(userId);
+    }
+
+    const savedPost = await post.save()
+    res.status(200).json({ 
+        message: hasLiked ? "Post unliked" : "Post liked", 
+        likesCount: post.likes.length 
+    })
+}
+catch (error) {
+    console.error("Error liking/unliking post:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+}
+
+}
+
 
 
 
